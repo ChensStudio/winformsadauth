c43b9dd8e1d965d6a5233de187edc714096b642c
Yang Chen <chenyangyinpeng@msn.com>
2021-08-07 22:33:31 -0700

    add code to complet bottom part of getdata

--- AuthDemoWinForms/Form1.cs
+++ AuthDemoWinForms/Form1.cs
@@ -6,13 +6,18 @@ using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using SC = System.Data.SqlClient;
+using TT = System.Threading.Tasks;
+using AD = Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+
 namespace AuthDemoWinForms
 {
     public partial class Form1 : Form
     {
         private static bool loggeIn = false;
         private static readonly string[] scopes = { "user.read" };
-        
+
         public Form1()
         {
             InitializeComponent();
@@ -106,23 +111,52 @@ namespace AuthDemoWinForms
 
         private void btnGetData_Click(object sender, EventArgs e)
         {
-            // User ID is optional for .NET Core and .NET Standard.
-            string ConnectionString = @"Server=tcp:mf-west-us-svr1.database.windows.net,1433;Initial Catalog=model-force-dev;Persist Security Info=False;MultipleActiveResultSets=False;
-                                                Encrypt=True;TrustServerCertificate=False;Authentication=Active Directory Integrated;User Id=ychen@chenyangyinpengmsn.onmicrosoft.com;";
+            if (label4.Text == "Anonymous")
+            {
+                MessageBox.Show("Please login first");
+                return;
+            }
 
-            //string ConnectionString = @"Server = mf-west-us-svr1.database.windows.net,1433; Authentication = Active Directory Integrated; Database = model-force-dev;";  
+            // User ID is optional for .NET Core and .NET Standard.
+            //string ConnectionString = @"Server=tcp:mf-west-us-svr1.database.windows.net,1433;Initial Catalog=model-force-dev;Persist Security Info=False;MultipleActiveResultSets=False;
+            //                                    Encrypt=True;TrustServerCertificate=False;Authentication=Active Directory Interactive;";
+ 
 
             // Provide the query string with a parameter placeholder.
             string queryString = "select Name, ListPrice, Weight "
                             + "from [SalesLT].[Product] "
                             + "where ProductID = 680";
 
+
+            SC.SqlConnectionStringBuilder builder = new SC.SqlConnectionStringBuilder();
+
+            // Program._  static values that you set earlier.
+            builder["Data Source"] = "tcp:mf-west-us-svr1.database.windows.net,1433";
+            builder.UserID = label4.Text;
+            builder["Initial Catalog"] = "model-force-dev";
+
+            // This "Password" is not used with .ActiveDirectoryInteractive.
+            //builder["Password"] = "<YOUR PASSWORD HERE>";
+
+            builder["Connect Timeout"] = 15;
+            builder["TrustServerCertificate"] = true;
+            builder.Pooling = false;
+
+            // Assigned enum value must match the enum given to .SetProvider().
+            builder.Authentication = SC.SqlAuthenticationMethod.ActiveDirectoryInteractive;
+
             // Create and open the connection in a using block. This
             // ensures that all resources will be closed and disposed
             // when the code exits.
             using (SqlConnection connection =
-                new SqlConnection(ConnectionString))
+                new SqlConnection(builder.ConnectionString))
             {
+                var provider = new ActiveDirectoryAuthProvider();
+
+                SC.SqlAuthenticationProvider.SetProvider(
+                    SC.SqlAuthenticationMethod.ActiveDirectoryInteractive
+                    ,provider);
+
                 // Create the Command and Parameter objects.
                 SqlCommand command = new SqlCommand(queryString, connection);
 
@@ -148,4 +182,69 @@ namespace AuthDemoWinForms
             }
         }
     }
+
+    public class ActiveDirectoryAuthProvider : SC.SqlAuthenticationProvider
+    {
+        static public readonly string ClientApplicationID = "acbdaf98-6949-45dc-bfc4-4de781e1317f";
+        static public readonly Uri RedirectUri = new Uri("https://login.microsoftonline.com/common/oauth2/nativeclient");
+
+        // Program._ more static values that you set!
+        private readonly string _clientId = ClientApplicationID;
+        private readonly Uri _redirectUri = RedirectUri;
+
+        public override async TT.Task<SC.SqlAuthenticationToken>
+            AcquireTokenAsync(SC.SqlAuthenticationParameters parameters)
+        {
+            AD.AuthenticationContext authContext =
+                new AD.AuthenticationContext(parameters.Authority);
+            authContext.CorrelationId = parameters.ConnectionId;
+            AD.AuthenticationResult result;
+
+            switch (parameters.AuthenticationMethod)
+            {
+                case SC.SqlAuthenticationMethod.ActiveDirectoryInteractive:
+                    Console.WriteLine("In method 'AcquireTokenAsync', case_0 == '.ActiveDirectoryInteractive'.");
+
+                    result = await authContext.AcquireTokenAsync(
+                        parameters.Resource,  // "https://database.windows.net/"
+                        _clientId,
+                        _redirectUri,
+                        new AD.PlatformParameters(AD.PromptBehavior.Auto),
+                        new AD.UserIdentifier(
+                            parameters.UserId,
+                            AD.UserIdentifierType.RequiredDisplayableId));
+                    break;
+
+                //case SC.SqlAuthenticationMethod.ActiveDirectoryIntegrated:
+                //    Console.WriteLine("In method 'AcquireTokenAsync', case_1 == '.ActiveDirectoryIntegrated'.");
+
+                //    result = await authContext.AcquireTokenAsync(
+                //        parameters.Resource,
+                //        _clientId,
+                //        new AD.UserCredential());
+                //    break;
+
+                //case SC.SqlAuthenticationMethod.ActiveDirectoryPassword:
+                //    Console.WriteLine("In method 'AcquireTokenAsync', case_2 == '.ActiveDirectoryPassword'.");
+
+                //    result = await authContext.AcquireTokenAsync(
+                //        parameters.Resource,
+                //        _clientId,
+                //        new AD.UserPasswordCredential(
+                //            parameters.UserId,
+                //            parameters.Password));
+                //    break;
+
+                default: throw new InvalidOperationException();
+            }
+            return new SC.SqlAuthenticationToken(result.AccessToken, result.ExpiresOn);
+        }
+
+        public override bool IsSupported(SC.SqlAuthenticationMethod authenticationMethod)
+        {
+            return authenticationMethod == SC.SqlAuthenticationMethod.ActiveDirectoryIntegrated
+                || authenticationMethod == SC.SqlAuthenticationMethod.ActiveDirectoryInteractive
+                || authenticationMethod == SC.SqlAuthenticationMethod.ActiveDirectoryPassword;
+        }
+    } // EOClass ActiveDirectoryAuthProvider.
 }
